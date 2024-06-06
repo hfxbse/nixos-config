9abf0500dc2374b01674323c6f8cee58533a0c4a
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-06-06 17:38:14 +0200

    set git user name to host user description

--- modules/text-processing.nix
+++ modules/text-processing.nix
@@ -1,8 +1,9 @@
-{ ... }: {
+{ host, ... }: {
   programs.git.enable = true;
   programs.git.config = {
     init.defaultBranch = "main";
     core.editor = "nvim";
+    user.name = host.user.description;
   };
 
   programs.neovim = {
