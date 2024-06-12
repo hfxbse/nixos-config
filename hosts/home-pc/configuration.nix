42fc5307b0657d3f7441997b4a9ec0bfd2535649
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-06-12 21:12:24 +0200

    update comment on top of configuration.nix files

--- hosts/home-pc/configuration.nix
+++ hosts/home-pc/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, host, ... }:
 {
   imports = [
@@ -50,16 +46,16 @@
 
   # Enable MTP mounting
   services.gvfs.enable = true;
-  
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
   services.printing.logLevel = "debug";
   services.printing.drivers = [
     (pkgs.callPackage ../../hardware/Brother/hl3172cdw.nix {})
-  ];  
+  ];
 
   services.avahi.nssmdns4 = false; # Use the settings from below
-  
+
   # settings from avahi-daemon.nix where mdns is replaced with mdns4
   system.nssModules = pkgs.lib.optional (!config.services.avahi.nssmdns4) pkgs.nssmdns;
   system.nssDatabases.hosts = with pkgs.lib; optionals (!config.services.avahi.nssmdns4) (mkMerge [
