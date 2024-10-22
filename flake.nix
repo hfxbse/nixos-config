da2623bd0fdaba3847cdeafdc353ea8eba089e64
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2024-10-23 01:11:30 +0200

    change working directory of pdflatex to main document directory

--- flake.nix
+++ flake.nix
@@ -32,16 +32,21 @@
 
       if [ $HELP = true ]; then
         echo "Options";
-        echo "-f <PATH>           File path to the LaTeX document entry point.";
+        echo "-f <PATH>           Path to the main LaTeX file.";
         echo "-h                  Shows this help.";
         echo "-o <PATH>           Compilation output directory path.";
         exit 1;
       fi
 
+      # Mirror the structure of the source directories
+      # pdflatex cannot write the output files otherwise
       DOC_DIR=$(dirname $DOC);
       for SRC_DIR in $(find $DOC_DIR -type d -not -path "$OUT*"); do
         mkdir -p "''${SRC_DIR/$DOC_DIR/$OUT}";
       done
+
+      # Required to get relative paths inside LaTeX to work
+      cd $DOC_DIR;
 
       ${latex}/bin/pdflatex -output-directory="$OUT" "$DOC";
       ${latex}/bin/biber --output-directory="$OUT" "$OUT"/*.bcf;
