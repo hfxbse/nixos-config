0f66e0f925f737a2776b8fc59fc7a5d5c8eff171
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2025-03-13 02:00:04 +0100

    init baseline flake template

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A baseline flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    defaultSystems = [ "x86_64-linux" ];
+
+    forAllSystems = systems: function: nixpkgs.lib.genAttrs systems (system: function system);
+    forAllDefaultSystems = forAllSystems defaultSystems;
+  in
+  {
+    packages = forAllDefaultSystems (system:
+    let
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      hello = pkgs.hello;
+      default = self.packages.${system}.hello;
+    });
+
+    templates = {
+      default = self.templates.baseline;
+      baseline = {
+        description = "A baseline flake";
+        path = ./.;
+      };
+    };
+  };
+}
