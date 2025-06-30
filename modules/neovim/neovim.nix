e2bb58f87f03a3a7035de1b924ff7f2e24356eaf
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2025-06-30 14:48:03 +0200

    Add Yeet to Neovim
    Allows to rerun simple task defined in bash

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -1,4 +1,22 @@
-{ lib, ... }:
+{
+  lib,
+  pkgs,
+  ...
+}:
+let
+  buildVimPlugin = pkgs.vimUtils.buildVimPlugin;
+  fetchFromGitHub = pkgs.fetchFromGitHub;
+
+  yeet = buildVimPlugin {
+    name = "yeet";
+    src = fetchFromGitHub {
+      owner = "samharju";
+      repo = "yeet.nvim";
+      rev = "31046346b4f146e337c3f1fda604f0e1e25e1df7";
+      hash = "sha256-4kUnzGEgvg6E9JDSeTZ9kqwN5QBO+wN4/wWo/x5zbaw=";
+    };
+  };
+in
 {
   imports = [
     ./theme.nix
@@ -18,6 +36,16 @@
         mode = [ "n" ];
         key = "<C-A-l>";
         action = "<cmd>lua vim.lsp.buf.format()<cr>";
+      }
+      {
+        mode = [ "n" ];
+        key = "tt";
+        action = "<cmd>vertical botright terminal<cr>";
+      }
+      {
+        mode = [ "n" ];
+        key = "bc";
+        action = "<cmd>%bd<cr>";
       }
     ];
 
@@ -39,5 +67,10 @@
       gitsigns.enable = true;
       trim.enable = true;
     };
+
+    extraPlugins = [ yeet ];
+    extraConfigLua = ''
+      require('yeet').setup({})
+    '';
   };
 }
