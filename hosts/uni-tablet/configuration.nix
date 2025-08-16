550cdc101169323b514d58d8e5cce98f5d09d8b6
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2025-08-16 03:04:33 +0200

    Add auto screen rotating extension for tablet

--- hosts/uni-tablet/configuration.nix
+++ hosts/uni-tablet/configuration.nix
@@ -1,10 +1,20 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   imports = [ ./disk-config.nix ];
   facter.reportPath = ./facter.json;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  hardware.sensor.iio.enable = true;
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.screen-rotate
+  ];
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org/gnome/shell]
+    enabled-extensions=['screen-rotate@shyzus.github.io']
+  '';
 
   user.name = "fxbse";
   desktop = {
