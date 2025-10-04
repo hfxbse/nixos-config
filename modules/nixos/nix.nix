93d89bb129a278050cac34f57c7e917cac1405a6
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2025-10-05 00:37:11 +0200

    Enable automatic system updates
    Also enables the automatic management of the Nix store

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,7 +1,36 @@
 { ... }:
+let
+  randomizedDelaySec = "10min";
+in
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    settings.auto-optimise-store = true;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    gc = {
+      automatic = true;
+      options = "--delete-older-than 60d";
+
+      persistent = true;
+      inherit randomizedDelaySec;
+    };
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    operation = "boot";
+
+    persistent = true;
+    inherit randomizedDelaySec;
+
+    flake = "github:hfxbse/nixos-config";
+    upgrade = false;  # Use versions pinned by the flake
+    flags = [
+      "--print-build-logs"
+      "--no-update-lock-file"
+    ];
+  };
 }
