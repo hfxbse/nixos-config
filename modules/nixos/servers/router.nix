2ba0741897b319f037657c1570aa333dd3d55358
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-03-03 21:51:57 +0100

    feat(server): Announce router to containers

--- modules/nixos/servers/router.nix
+++ modules/nixos/servers/router.nix
@@ -1,10 +1,22 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.server.router // {
     enable = lib.pipe config.server.services [
       builtins.attrValues
       (lib.any (server: server.enable or false))
     ];
+  };
+
+  resolverFix = {
+    # Use systemd-resolved inside the containers
+    # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+    networking.useHostResolvConf = lib.mkForce false;
+    services.resolved.enable = lib.mkDefault true;
   };
 
   types = lib.types;
@@ -18,10 +30,25 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    virtualisation.vmVariant.virtualisation.qemu.networkingOptions = lib.mkForce [
-      "-net nic,netdev=user.0,model=virtio"
-      "-netdev user,id=user.0,ipv6-prefix=${ulaPrefix}::,ipv6-prefixlen=48,\"$QEMU_NET_OPTS\""
-    ];
+    virtualisation.vmVariant = {
+      # By default deprecated side-local addresses are chosen by QEMU
+      # When setting an ULA, internet connectivity breaks because of this
+      # Forcing QEMU to use an ULA as well fixes this.
+      virtualisation.qemu.networkingOptions = lib.mkForce [
+        "-net nic,netdev=user.0,model=virtio"
+        "-netdev user,id=user.0,ipv6-prefix=${ulaPrefix}:0::,ipv6-prefixlen=64,\"$QEMU_NET_OPTS\""
+      ];
+
+      # Enable NAT66 when running as VM as their ain't any global IPv6 address
+      containers.router.config.networking.nftables.ruleset = ''
+        table ip6 nat {
+          chain postrouting {
+            type nat hook postrouting priority srcnat; policy accept;
+            iifname "eth0" oifname "mv-${cfg.wan}" masquerade
+          }
+        }
+      '';
+    };
 
     networking.firewall.enable = true;
     networking.nftables.enable = true;
@@ -34,6 +61,14 @@ in
           Name = bridgeName;
         };
       };
+
+      networks."41-${bridgeName}" = {
+        matchConfig.Name = bridgeName;
+        networkConfig = {
+          ConfigureWithoutCarrier = true;
+          IPv6AcceptRA = false;
+        };
+      };
     };
 
     server.containerNames = [ "router" ];
@@ -43,20 +78,37 @@ in
         macvlans = [ cfg.wan ];
         hostBridge = bridgeName;
 
-        config = {
-          networking.hostName = "${config.networking.hostName}-router";
-          systemd.network = {
-            enable = true;
-            networks."10-wan" = {
-              matchConfig.Name = "mv-${cfg.wan}";
-              networkConfig.DHCP = true;
-            };
+        config = lib.recursiveUpdate resolverFix {
+          boot.kernel.sysctl = {
+            "net.ipv6.conf.all.forwarding" = 2;
           };
 
-          # Use systemd-resolved inside the container
-          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-          networking.useHostResolvConf = lib.mkForce false;
-          services.resolved.enable = lib.mkDefault true;
+          networking.hostName = "${config.networking.hostName}-router";
+          networking.nftables.enable = true;
+
+          systemd.network = {
+            enable = true;
+            networks = {
+              "10-wan" = {
+                matchConfig.Name = "mv-${cfg.wan}";
+                networkConfig = {
+                  DHCP = true;
+                  IPv6AcceptRA = true;
+                };
+              };
+
+              "30-${bridgeName}" = {
+                address = [ "${ulaPrefix}:1::1/64" ];
+                matchConfig.Name = "eth*";
+                networkConfig = {
+                  DHCPPrefixDelegation = true;
+                  IPv6SendRA = true;
+                  IPv6AcceptRA = false;
+                };
+                ipv6Prefixes = [ { Prefix = "${ulaPrefix}:1::/64"; } ];
+              };
+            };
+          };
         };
       };
     }
@@ -66,6 +118,16 @@ in
         names:
         lib.genAttrs names (name: {
           inherit (config.containers.router) privateNetwork hostBridge;
+          config = lib.recursiveUpdate resolverFix {
+            environment.systemPackages = with pkgs; [ traceroute ];
+            systemd.network = {
+              enable = true;
+              networks."30-${bridgeName}" = {
+                matchConfig.Name = "eth*";
+                networkConfig.DHCP = true;
+              };
+            };
+          };
         })
       )
     ];
