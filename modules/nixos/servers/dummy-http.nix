86dae7b32e232158333c0a23d0b506913284af50
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-03-04 21:01:07 +0100

    feat(server): Implement HTTP service discovery via mDNS

--- modules/nixos/servers/dummy-http.nix
+++ modules/nixos/servers/dummy-http.nix
@@ -12,9 +12,17 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    server.services.reverse-proxy.containerNames = [ "dummy-http" ];
+    # Can be tested using curl
+    # curl http://example.com --resolve 'example.com:80:127.0.0.1'
+    # This tells curl to resolve the domain to the reverse-proxy's IP
+    server.services.reverse-proxy.virtualHosts."example.com" = {
+      containerName = "dummy-http";
+      port = 80;
+    };
+
     containers.dummy-http = {
       config = {
+        networking.firewall.allowedTCPPorts = [ 80 ];
         services.httpd.enable = true;
       };
     };
--- modules/nixos/servers/reverse-proxy.nix
+++ modules/nixos/servers/reverse-proxy.nix
@@ -1,21 +1,48 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
+  inherit (lib) types;
+
   cfg = config.server.services.reverse-proxy;
   bridgeName = "br-http";
   ulaPrefix = "fd00:9d1f:b7b7:1721";
   veth = "vb-http";
+
+  containerNames = lib.pipe cfg.virtualHosts [
+    builtins.attrValues
+    (map ({ containerName, ... }: containerName))
+  ];
 in
 {
-  options.server.services.reverse-proxy.containerNames = lib.mkOption {
-    description = "Names of the containers behind the reverse-proxy";
-    default = [];
+  options.server.services.reverse-proxy.virtualHosts = lib.mkOption {
+    description = "Virtual hosts served the reverse-proxy";
+    default = { };
+
+    type = types.attrsOf (
+      types.submodule (
+        { ... }:
+        {
+          options = {
+            port = lib.mkOption {
+              description = "On which port the service is listening";
+              type = types.ints.between 0 65535;
+            };
+
+            containerName = lib.mkOption {
+              description = "Name of the container running the virtual host";
+              type = types.str;
+            };
+          };
+        }
+      )
+    );
   };
 
-  config = lib.mkIf (builtins.length cfg.containerNames != 0) {
+  config = lib.mkIf (builtins.length containerNames != 0) {
     systemd.network = {
       enable = true;
       netdevs."25-${bridgeName}" = {
@@ -31,16 +58,18 @@ in
       };
     };
 
-    server.containerNames = [ "reverse-proxy" ] ++ cfg.containerNames;
+    server.containerNames = [ "reverse-proxy" ] ++ containerNames;
     containers = {
       reverse-proxy = {
         extraVeths.${veth}.hostBridge = bridgeName;
 
         config = {
+          environment.systemPackages = with pkgs; [ dnsutils ];
           boot.kernel.sysctl = {
             "net.ipv6.conf.all.forwarding" = 2;
           };
 
+          services.resolved.settings.Resolve.MulticastDNS = "resolve";
           systemd.network = {
             enable = true;
             networks."40-${veth}" = {
@@ -51,10 +80,13 @@ in
               networkConfig = {
                 IPv6SendRA = true;
                 IPv6AcceptRA = false;
+                MulticastDNS = "resolve";
               };
             };
           };
 
+          # NAT66 for the HTTP network
+          # Provides internet access without assigning public IPv6 addresses
           networking.nftables = {
             enable = true;
             ruleset = ''
@@ -67,13 +99,55 @@ in
             '';
           };
 
+          # mDNS
+          networking.firewall.interfaces.${veth}.allowedUDPPorts = [ 5353 ];
+          networking.firewall.allowedTCPPorts = [
+            80 # HTTP
+            443 # HTTPS
+          ];
+
           services.haproxy.enable = true;
-          services.haproxy.config = "";
+          services.haproxy.config = ''
+            resolvers sys
+              parse-resolv-conf
+            frontend www
+              mode http
+              bind :::80
+              use_backend %[req.hdr(Host),lower]
+          ''
+          + lib.pipe cfg.virtualHosts [
+            (lib.mapAttrsToList (
+              domain: cfgHost:
+              let
+                serverName = "container_${cfgHost.containerName}";
+                origin = "${cfgHost.containerName}.local:${toString cfgHost.port}";
+              in
+              # Do not use the internel libc resolver of haproxy
+              # The initial address resolution messes up mDNS completely
+              ''
+                backend ${domain}
+                  mode http
+                  server ${serverName} ${origin} resolvers sys init-addr last,none
+              ''
+            ))
+            (lib.concatStringsSep "\n\n")
+          ];
         };
       };
     }
-    // lib.genAttrs cfg.containerNames (name: {
+    // lib.genAttrs containerNames (name: {
       hostBridge = bridgeName;
+      config = {
+        services.resolved.settings.Resolve.MulticastDNS = true;
+
+        systemd.network = {
+          enable = true;
+          networks."20-mDNS" = {
+            matchConfig.Name = "eth*";
+            networkConfig.MulticastDNS = true;
+          };
+        };
+      };
     });
   };
 }
