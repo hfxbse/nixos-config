ed59e8504a476f6a8cd5f347789a77f7c7d27837
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-07-07 22:01:35 +0200

    feat(ice-skate): Re-enable ipu6 drivers
    No longer breaks system sleep since kernel 6.18.
    But still no support for OV8856 sensors.
    See: https://github.com/intel/ipu6-drivers/issues/23

--- hosts/ice-skate/configuration.nix
+++ hosts/ice-skate/configuration.nix
@@ -1,6 +1,5 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
@@ -25,11 +24,8 @@
     "amdgpu.ppfeaturemask=0xffffffff"
   ];
 
-  # Regression bug for ipu6 starting with Linux 6.16
-  # See https://github.com/intel/ipu6-drivers/issues/381
-  # The issue prevents the device to be suspended or rebootet properly.
   hardware.facter.reportPath = ./facter.json;
-  hardware.ipu6.enable = lib.mkForce false;
+  hardware.ipu6.videoDeviceNumber = 99;
 
   user.name = "fxbse";
   networking.hostName = "ice-skate";
