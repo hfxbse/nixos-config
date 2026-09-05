0c0eb2c66119aaba1d103055cbafb6a1cc0f95e6
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-09-05 22:16:27 +0200

    Add git indicators

--- modules/nixvim/color-scheme.nix
+++ modules/nixvim/color-scheme.nix
@@ -1,28 +1,14 @@
 { pkgs, ... }:
-let
-  inherit (pkgs) fetchFromGitHub vimUtils;
-  jb = vimUtils.buildVimPlugin {
-    name = "jb.nvim";
-    src = fetchFromGitHub {
-      owner = "nickkadutskyi";
-      repo = "jb.nvim";
-      rev = "984139426dbc1af955fab83b964712d36db7213d";
-      hash = "sha256-rlS/kEwPWMDynTmFAWaBkIJlPFdPBm89pClJsUBz5P0=";
-    };
-  };
-in
 {
-  extraPlugins = [ jb ];
-  extraConfigLua = ''
-    vim.cmd("colorscheme jb")
-  '';
-
   opts = {
     termguicolors = true;
   };
 
+  colorschemes.one.enable = true;
   plugins.treesitter = {
     enable = true;
     settings.highlight.enable = true;
   };
+
+  extraPlugins = with pkgs.vimPlugins; [ auto-dark-mode-nvim ];
 }
