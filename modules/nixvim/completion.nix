c7aa5c67e2e12a9ae49967f960a735c06ca8e34f
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-09-21 16:19:31 +0200

    feat(nixvim): Auto match parentheses and tags

--- modules/nixvim/completion.nix
+++ modules/nixvim/completion.nix
@@ -1,11 +1,22 @@
+{ pkgs, ... }:
 {
-  plugins.blink-cmp = {
-    enable = true;
-    settings = {
-      keymap = {
-        preset = "default";
-        "<C-j>" = [ "select_and_accept" ];
+  plugins = {
+    blink-cmp = {
+      enable = true;
+      settings = {
+        keymap = {
+          preset = "default";
+          "<C-j>" = [ "select_and_accept" ];
+        };
       };
     };
+    endwise.enable = true;
+    ts-autotag.enable = true;
   };
+
+  extraPlugins = with pkgs.vimPlugins; [ ultimate-autopair-nvim ];
+  extraConfigLua = ''
+    require('ultimate-autopair').setup({
+    })
+  '';
 }
