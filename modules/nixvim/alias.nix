78031d59a286e411a8adb8cab4e3db242ac849d3
Fabian Haas <29468630+hfxbse@users.noreply.github.com>
2026-09-23 14:30:19 +0200

    refactor: Create subflake for nixvim

---
+++ modules/nixvim/alias.nix
@@ -0,0 +1,3 @@
+{
+  viAlias = true;
+}
---
+++ modules/nixvim/flake.nix
@@ -0,0 +1,7 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+  };
+}
